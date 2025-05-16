4b74bd595d75669c2734f55b9294ef2456ee6f87
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-05-16 19:55:19 +0530

    Revert "Complete User Auth cmplete in backend in laravel "

--- students_backend/app/Models/User.php
+++ students_backend/app/Models/User.php
@@ -2,19 +2,31 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
+
 
     protected $fillable = [
-        'index_number', 'role', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+    ];
+
+  
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 }
