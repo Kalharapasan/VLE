49477ffbb74ad8838e8e14ffc14551e37e55cbdb
MiharaKGL <lasandimihara2@gmail.com>
2025-06-02 17:43:09 +0530

    comment

--- students_frontend/src/components/Pages/AboutPage.jsx
+++ students_frontend/src/components/Pages/AboutPage.jsx
@@ -6,8 +6,9 @@ const AboutPage = () => {
         <div className="container py-5">
             <h1 className="mb-4">Welcome to the Student Learning System</h1>
             <p>This is the home page of your university management system. Use the navigation above to explore more.</p>
+
         </div>
     );
 };
-
+//my comment Maya
 export default AboutPage;
