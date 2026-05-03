5a65ad8ba6380be307a4aa3bbd736d6e12cfde4f
Pasan Kalhara <kalharap84@gmail.com>
2026-05-03 20:57:55 +0530

    feat: Add create, update, and delete functionalities for timetable services

--- frontend/src/components/Service/Teacher/teacherService.js
+++ frontend/src/components/Service/Teacher/teacherService.js
@@ -63,3 +63,9 @@ export const deleteStudentMarks = (id) => api.delete(`/studentExamMarks/${id}`);
 
 // Timetable Services
 export const getTimeTable = () => api.get('/timeTable');
+export const createTimeTable = (data) => api.post('/timeTable', data);
+export const updateTimeTable = (id, data) => {
+    data.append('_method', 'PUT');
+    return api.post(`/timeTable/update/${id}`, data);
+};
+export const deleteTimeTable = (id) => api.delete(`/timeTable/${id}`);
